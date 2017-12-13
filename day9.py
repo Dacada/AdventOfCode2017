d18dcb99e59c63484a352347f9e623a6fca2dce5
David Carrera <donpolilla@gmail.com>
2017-12-13 15:14:37 +0100

    Finished day 9

--- day9.py
+++ day9.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python
 # -*- encoding:utf-8 -*-
 
-input = open("input9.txt").read()
+input = open("input9.txt").read().strip()
 
 def parse(input):
     assert input[0] == '{' and input[-1] == '}'
     input = input[1:-1]
     
     stack = [[]]
+    state = 'read'
     
     for c in input:
-        if c == '{':
-            stack.append([])
-        else if c == '}':
-            stack[-1].append(stack.pop())
+        if state == 'read':
+            if c == '{':
+                stack.append([])
+            elif c == '}':
+                current_group = stack.pop()
+                stack[-1].append(current_group)
+            elif c == '<':
+                state = 'garbage'
+        elif state == 'garbage':
+            if c == '>':
+                state = 'read'
+            elif c == '!':
+                state = 'ignore'
+        elif state == 'ignore':
+            state = 'garbage'
 
     return stack.pop()
 
+def score(group, acc):
+    return 1 + acc + sum(score(g, acc+1) for g in group)
+
 def run(input):
     groups = parse(input)
-    return score(groups)
+    return score(groups, 0)
 
 def main():
     new_input = raw_input("> ")
