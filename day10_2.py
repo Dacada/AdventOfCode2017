87e1d13ff08fcb9febf73e0c1751e7cf45542e16
dacada <donpolilla@gmail.com>
2017-12-14 14:41:27 +0100

    started day 10 part 2

---
+++ day10_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- encoding:utf-8 -*-
+
+import day10
+
+
+
+def finish_hash(list):
+    result = ""
+    for i in range(16):
+        dense = xor(list[i*16:(i-1)*16])
+        result += hex(dense&0x100)[3:]
+
+def run(input):
+    lengths = [ord(c) for c in input.strip()]
+    lengths.extend((17, 31, 73, 47, 23))
+    current_position = 0
+    skip_size = 0
+    for __ in range(64):
+        current_position,skip_size = hash(lengths, current_position, skip_size)
+    return finish_hash(day10.list)
+    
+def main():
+    new_input = raw_input("> ")
+    if new_input:
+        print run(new_input)
+    else:
+        print run(day10.input)
+
+if __name__ == "__main__":
+    main()
