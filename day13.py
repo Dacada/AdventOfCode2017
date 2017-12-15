2b781959da1c8651db372aed52c82abf9b74bae8
dacada <donpolilla@gmail.com>
2017-12-15 19:15:35 +0100

    Working on day13...

--- day13.py
+++ day13.py
@@ -47,16 +47,64 @@ input = """0: 3
 84: 30
 88: 14"""
 
+class Layer(object):
+    def __init__(self, range):
+        self.range = range
+        self.current = 0
+
+    def tick(self):
+        self.current += 1
+        if self.current == self.range:
+            self.current = 0
+
+class Firewall(object):
+    def __init__(self, definition):
+        self._layers = {}
+        for i in definition:
+            self._layers[i] = Layer(definition[i])
+        self.player_position = 0
+
+    def player_caught(self):
+        return self.player_position in self._layers and self._layers[self.player_position] == 0
+
+    def get_severity(self):
+        return self.player_position * self._layers[self.player_position].range
+
+    def tick(self):
+        self.player_position += 1
+        for layer in self._layers.values():
+            layer.tick()
+
+    def run(self):
+        end = max(self._layers)
+        severity = 0
+        
+        while self.player_position <= end:
+            if self.player_caught():
+                severity += self.get_severity()
+            self.tick()
+
+        return severity
+
+def parse(input):
+    d = {}
+    for line in input.split('\n'):
+        if line:
+            layer,range = line.split(':')
+            d[int(layer)] = int(range.strip())
+    return Firewall(d)
+
 def run(input):
-    pass
+    firewall = parse(input)
+    return firewall.run()
 
 def main():
     sys.stdout.write("> ")
     new_input = sys.stdin.read()
     if new_input:
-        run(new_input)
+        print run(new_input)
     else:
-        run(input)
+        print run(input)
 
 if __name__ == '__main__':
     main()
