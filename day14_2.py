8999d0d9bc4c35c117d275ef642424c681a146fa
David Carrera <donpolilla@gmail.com>
2017-12-16 21:08:06 +0100

    Finished day 14 part 2

---
+++ day14_2.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- encoding:utf-8 -*-
+
+import day10_2
+import day14
+
+class Cell(object):
+    def __init__(self, coords):
+        self.neighbors = []
+        self.coords = coords
+    def neighbor(self, other):
+        self.neighbors.append(other)
+    def connects_to(self, other):
+        return self._connects_to(other, set())
+    def _connects_to(self, other, seen):
+        if self is other:
+            return True
+        else:
+            seen.add(self)
+            for cell in self.neighbors:
+                if cell not in seen:
+                    if cell._connects_to(other, seen):
+                        return True
+            return False
+    def __hash__(self):
+        return hash(self.coords)
+
+class Memory(object):
+    def __init__(self, bits):
+        self._cells = {}
+        for y in range(128):
+            for x in range(128):
+                if bits[y][x] == '1':
+                    self._cells[(x,y)] = Cell((x,y))
+
+    def link_up(self):
+        for coord,cell in self._cells.items():
+            up_coord = (coord[0], coord[1] - 1) if coord[1] - 1 >= 0 else None
+            down_coord = (coord[0], coord[1] + 1) if coord[1] + 1 < 128 else None
+            left_coord = (coord[0] - 1, coord[1]) if coord[0] - 1 >= 0 else None
+            right_coord = (coord[0] + 1, coord[1]) if coord[0] + 1 < 128 else None
+
+            if up_coord in self._cells:
+                cell.neighbor(self._cells[up_coord])
+            if down_coord in self._cells:
+                cell.neighbor(self._cells[down_coord])
+            if left_coord in self._cells:
+                cell.neighbor(self._cells[left_coord])
+            if right_coord in self._cells:
+                cell.neighbor(self._cells[right_coord])
+
+    def count_regions(self):
+        seen = set()
+        count = 0
+        for coord,cell in self._cells.items():
+            if coord not in seen:
+                empty = True
+                for coord2,cell2 in self._cells.items():
+                    if coord2 not in seen:
+                        if cell.connects_to(cell2):
+                            seen.add(coord2)
+                            empty = False
+                if not empty:
+                    count += 1
+
+        return count
+
+def run(input):
+    bits = []
+    
+    for i in range(128):
+        hash_input = input + '-' + str(i)
+        hash = day10_2.run(hash_input)
+        bits.append([b for b in bin(int('1'+hash,16))[3:]])
+        day10_2.day10.list = range(256)
+        
+    memory = Memory(bits)
+    memory.link_up()
+    return memory.count_regions()
+
+day14.run = run
+
+if __name__ == '__main__':
+    day14.main()
