0ae80be785bd8eaf190144533e040b786e22392c
David Carrera <donpolilla@gmail.com>
2017-12-18 15:09:28 +0100

    Continuing day 18

--- day18.py
+++ day18.py
@@ -2,17 +2,42 @@
 # -*- encoding:utf-8 -*-
 
 import sys
+import day8
 
 input = open("input18.txt").read()
+
+class Program(object):
+    def __init__(self, instructions):
+        self._registers = day8.RegisterBank()
+        self.instructions = instructions
+        self.program_counter = 0
+
+    def run(self):
+        pass
+
+    def _snd(self, x):
+        pass
+
+    def _set(self, x, y):
+        pass
+
+    
+
+def to_lists(input):
+    return [instr.split() for instr in input.split('\n')]
+
+def run(input):
+    program = Program(to_lists(input))
+    return program.run()
 
 def main():
     sys.stdout.write("> ")
     new_input = sys.stdin.read()
 
     if new_input:
-        run(new_input)
+        print run(new_input)
     else:
-        run(input)
+        print run(input)
 
 if __name__ == "__main__":
     main()
