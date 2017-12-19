eba1f33f04d024415e60b76bcd9446ef7dd14d27
dacada <donpolilla@gmail.com>
2017-12-19 14:26:42 +0100

    working on day 19

--- day19.py
+++ day19.py
@@ -3,6 +3,34 @@
 
 input = 'input19.txt'
 
+class Router(object):
+    class __init__(self, grid):
+        self._grid = [[cell for cell in row] for row in grid.split('\n')]
+        self.letters = ""
+        x = self._grid[0].index('|')
+        self.coords = (x,0)
+        self._direction = 's'
+        self._direction_meanings = {
+            'n' : ( 0,-1),
+            's' : ( 0, 1),
+            'e' : (-1, 0),
+            'w' : ( 1, 0)
+        }
+
+    def traverse(self):
+        next = self.next_direction()
+        while next is not None:
+            self.advance(next)
+            next = self.next_direction,()
+
+    def next_direction(self):
+        
+
+def run(input):
+    router = Router(open(input).read().strip())
+    router.traverse()
+    return router.letters
+
 def main():
     new_input = raw_input("> ")
     if new_input:
