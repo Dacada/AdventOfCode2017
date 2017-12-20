e5e02fb001d497c7138027a18ac5ebdc27e8b6e8
David Carrera <donpolilla@gmail.com>
2017-12-20 22:00:19 +0100

    Fixed day 4 style

--- day4.py
+++ day4.py
@@ -1,31 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- encoding:utf-8 -*-
 
-input = open('input4.txt').read()
+import base
 
-def is_valid(sentence):
-    words = sentence.split()
-    s = set()
-    for word in words:
-        if word in s:
-            return False
-        s.add(word)
-    return True
-
-def valid_count(input):
-    input_list = [s for s in input.split('\n') if s]
-    count = 0
-    for sentence in input_list:
-        if is_valid(sentence):
-            count += 1
-    return count
-
-def main():
-    new_input = raw_input("> ")
-    if new_input:
-        print is_valid(new_input)
-    else:
-        print valid_count(input)
+class Day(base.Base):
+    def is_valid(self, sentence):
+        words = sentence.split()
+        s = set()
+        for word in words:
+            if word in s:
+                return False
+            s.add(word)
+        return True
+    
+    def parse(self, input):
+        return [s for s in input.split('\n') if s]
+    
+    def run(self, input):
+        count = 0
+        for sentence in input:
+            if self.is_valid(sentence):
+                count += 1
+        return count
 
 if __name__ == "__main__":
-    main()
+    Day(4).main()
--- day4_2.py
+++ day4_2.py
@@ -1,21 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- encoding:utf-8 -*-
 
 import day4
 
-def is_anagram(word1, word2):
-    return ''.join(sorted(word1)) == ''.join(sorted(word2))
+class Day(day4.Day):
+    def is_anagram(self, word1, word2):
+        return ''.join(sorted(word1)) == ''.join(sorted(word2))
 
-def is_valid(sentence):
-    words = sentence.split()
-    for i in range(len(words)):
-        for j in range(len(words)):
-            if (i != j):
-                if is_anagram(words[i], words[j]):
-                    return False
-    return True
-
-day4.is_valid = is_valid
+    def is_valid(self, sentence):
+        words = sentence.split()
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if (i != j):
+                    if self.is_anagram(words[i], words[j]):
+                        return False
+        return True
 
 if __name__ == '__main__':
-    day4.main()
+    Day(4).main()
