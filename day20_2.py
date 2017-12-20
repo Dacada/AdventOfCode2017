ba4153cfb22b0371eff05045dcb18595f0aa3dda
David Carrera <donpolilla@gmail.com>
2017-12-20 15:10:36 +0100

    Started day 20 part 2

---
+++ day20_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- encoding:utf-8 -*-
+
+import day0
+
+class Day(day0.Day):
+    def parse(self, input):
+        pass
+        
+    def run(self, input):
+        pass
+
+if __name__ == '__main__':
+    Day(0).main()
