9dde6b9a60bece6b2abeff9bcfe313950bd32302
David Carrera <donpolilla@gmail.com>
2017-12-20 22:19:13 +0100

    Improved day6 style

--- day6.py
+++ day6.py
@@ -1,42 +1,38 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- encoding:utf-8 -*-
 
-input = "4 1 15 12 0 9 9 5 5 8 7 3 14 5 12 3"
+import base
 
-def redistribute(registers):
-    max_n = max(registers)
-    max_i = registers.index(max_n)
-    registers[max_i] = 0
+class Day(base.Base):
+    def redistribute(self, registers):
+        max_n = max(registers)
+        max_i = registers.index(max_n)
+        registers[max_i] = 0
 
-    i = max_i + 1
-    while max_n:
-        if i >= len(registers):
-            i = 0
-        registers[i] += 1
-        i += 1
-        max_n -= 1
+        i = max_i + 1
+        while max_n:
+            if i >= len(registers):
+                i = 0
+            registers[i] += 1
+            i += 1
+            max_n -= 1
 
-def run(input):
-    registers = [int(n) for n in input.split() if n]
-    seen_states = set()
-    cycles = 0
+    def parse(self, input):
+        return [int(n) for n in input.split() if n]
 
-    while True:
-        redistribute(registers)
-        cycles += 1
-        state = tuple(registers)
-        if state in seen_states:
-            break
-        seen_states.add(state)
+    def run(self, registers):
+        seen_states = set()
+        cycles = 0
 
-    return cycles
+        while True:
+            self.redistribute(registers)
+            cycles += 1
+            state = tuple(registers)
+            if state in seen_states:
+                break
+            seen_states.add(state)
 
-def main():
-    new_input = raw_input("> ")
-    if new_input:
-        print run(new_input)
-    else:
-        print run(input)
+        return cycles
 
 if __name__ == "__main__":
-    main()
+    Day(6).main()
--- day6_2.py
+++ day6_2.py
@@ -1,20 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- encoding:utf-8 -*-
 
 import day6
 
-def run(input):
-    registers = [int(n) for n in input.split() if n]
-    seen_states = []
+class Day(day6.Day):
+    def run(self, registers):
+        seen_states = []
 
-    while True:
-        day6.redistribute(registers)
-        state = tuple(registers)
-        if state in seen_states:
-            return len(seen_states[seen_states.index(state):])
-        seen_states.append(state)
-
-day6.run = run
+        while True:
+            self.redistribute(registers)
+            state = tuple(registers)
+            if state in seen_states:
+                return len(seen_states[seen_states.index(state):])
+            seen_states.append(state)
 
 if __name__ == '__main__':
-    day6.main()
+    Day(6).main()
